6334c359d983e868b0f46eb08924c3b1e09b83d0
Paulo Roberto Sampaio Bezerra <bezerra.paulo@gmail.com>
2020-06-12 10:47:00 -0400

    Mudando link de compartilhamento para https

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@ const app = new Vue({
     message: "",
     blockRegister: false,
     txtConvite:
-      "https://api.whatsapp.com/send?text=Venha+jogar+comigo+o+Jogo+da+Velha+mais+Rocks+da+internet+em+http%3A%2F%2Fvelha.rocks&rlz=1C1GCEA_enBR808BR808&oq=whatsapp%3A%2F%2Fsend%3Ftext%3DVenha+jogar+comigo+o+Jogo+da+Velha+mais+Rocks+da+internet+em+http%3A%2F%2Fvelha.rocks",
+      "https://api.whatsapp.com/send?text=Venha+jogar+comigo+o+Jogo+da+Velha+mais+Rocks+da+internet+em+http%3A%2F%2Fvelha.rocks&rlz=1C1GCEA_enBR808BR808&oq=whatsapp%3A%2F%2Fsend%3Ftext%3DVenha+jogar+comigo+o+Jogo+da+Velha+mais+Rocks+da+internet+em+https%3A%2F%2Fvelha.rocks",
   },
   methods: {
     startGame() {
