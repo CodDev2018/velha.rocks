69cca7efd385bee1378b030942346741cf515186
Paulo Roberto Sampaio Bezerra <bezerra.paulo@gmail.com>
2020-06-12 11:00:50 -0400

    redirecionando https

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const server = http.Server(app).listen(3000);
 const io = socketIo(server);
 const clients = {};
 
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+  if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP 
+      res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+  else //Se a requisição já é HTTPS 
+      next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+});
+
 app.use(express.static("./public"));
 app.use("/vendor", express.static("./node_modules"));
 
